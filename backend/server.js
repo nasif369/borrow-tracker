2a5df4bdd900461ffdb34873f70acac327ee44e8
nasif369 <nasiftk5j@gmail.com>
2026-09-16 23:11:50 +0530

    Fix server port for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,14 @@ app.get("/", (req, res) => {
     res.send("Borrow Tracker Backend is working!");
 });
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected!");
 
-        app.listen(5000, () => {
-            console.log("Server running on port 5000");
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
         });
     })
     .catch((error) => {
